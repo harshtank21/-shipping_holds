a24b65a2c7242b6ff17889b95f081f2f42d474c7
harsh <tankharsh440@gmail.com>
2024-01-25 19:42:43 +0530

    [add tasks]

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,10 +7,18 @@ class SaleOrder(models.Model):
     shipping_hold = fields.Boolean("Shipping Hold")
 
     def action_confirm(self):
-        res= super().action_confirm()
-        if self.shipping_hold:
-            record=self.env["stock.picking"].search([("origin", "=", self.name)])
+        res = super().action_confirm()
+        if self.shipping_hold == True:
+            record = self.env["stock.picking"].search([("origin", "=", self.name)])
             print(record.state)
             if record.state != "done":
                 record.shipping_hold = True
+        return res
+
+    def write(self, values):
+        res = super().write(values)
+        if self.shipping_hold == False:
+            record = self.env["stock.picking"].search([("origin", "=", self.name)])
+            if record.state != "done":
+                record.shipping_hold = False
         return res
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,7 +1,15 @@
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     shipping_hold = fields.Boolean("Shipping Hold")
+
+    def button_validate(self):
+        if self.shipping_hold == True:
+            raise ValidationError(
+                f"This stock movement is currently on Shipping Hold. Its associated Sales Order {self.origin} must be taken off hold before it can be validated")
+
+        return super().button_validate()
