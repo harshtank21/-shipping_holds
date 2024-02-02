94377f715261bcf2803244eb11cbc08e780a7b8d
harsh <tankharsh440@gmail.com>
2024-02-02 19:19:31 +0530

    [add wizard]

--- models/sale_order.py
+++ models/sale_order.py
@@ -9,8 +9,7 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         res = super().action_confirm()
         if self.shipping_hold == True:
-            record = self.env["stock.picking"].search([("origin", "=", self.name)])
-            print(record.state)
+            record = self.picking_ids
             if record.state != "done":
                 record.shipping_hold = True
         return res
@@ -18,7 +17,7 @@ class SaleOrder(models.Model):
     def write(self, values):
         res = super().write(values)
         if self.shipping_hold == False:
-            record = self.env["stock.picking"].search([("origin", "=", self.name)])
+            record = self.picking_ids
             if record.state != "done":
                 record.shipping_hold = False
         return res
